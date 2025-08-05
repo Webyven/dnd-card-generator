7655fee196a7f19ed5e8dcf2e448cb6797a804c5
Webyven <nicoyurec@gmail.com>
2025-08-05 00:05:56 -0300

    Eliminación de directivas `using` innecesarias
    Se han eliminado varias directivas `using` en los archivos `CardControl.cs` y `fMain.cs`, simplificando el código y posiblemente reflejando cambios en las funcionalidades y dependencias del proyecto.

--- fMain.cs
+++ fMain.cs
@@ -1,13 +1,7 @@
-﻿using Guna.UI2.WinForms.Suite;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SpellCardsDnDGenerator
